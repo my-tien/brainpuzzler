b30ad3b961836a418b130596aba70778bbe74254
my-tien <adtieni@gmail.com>
2015-02-13 16:34:21 +0100

    accept all characters for campaign ids and mw ids

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, url
 from jobs import views
 
 urlpatterns = patterns('',
-                       url(r'^job_(?P<job_id>\d+)/camp_(?P<campaign_id>\w+)/mw_(?P<worker_id>\w+)/$', views.job, name='job'),
+                       url(r'^job_(?P<job_id>\d+)/camp_(?P<campaign_id>\.+)/mw_(?P<worker_id>\.+)/$', views.job, name='job'),
                        url(r'^job_(?P<job_id>\d+)/submit/$', views.job_submit, name='job_submit'),
                        url(r'index.html', views.index, name='index'),
                        )
