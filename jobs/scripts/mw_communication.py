2540f48e4c78c3557b46ace0f0b61f0852819492
my-tien <adtieni@gmail.com>
2015-02-21 15:53:05 +0100

    mw task get vcode

--- jobs/scripts/mw_communication.py
+++ jobs/scripts/mw_communication.py
@@ -1,31 +1,53 @@
 import json
 from pprint import pprint
 
-from jobs.MW_API import MW_API
+from scripts.MW_API import MW_API
 
 
 mw_api = MW_API('af801b23166b3a7a80df6eaf7efa3031095d16e69572f910a5c88bf2f6cf7fd9')
 
+
 class Campaign:
-
     def __init__(self, campaign_id):
-        self.campaign_id = campaign_id 
-
+        self.campaign_id = campaign_id
 
     def get_tasks(self):
-        return mw_api.do_request('GET', '/campaign_hg/list_tasks/' + self.campaign_id)
-    
+        return mw_get('GET', '/campaign_hg/list_tasks/' + self.campaign_id)
 
     def get_unrated_tasks(self):
-        tasks = get_tasks()
-        if tasks["status"] is "SUCCESS":
+        tasks = self.get_tasks()
+        if tasks is not None:
             return [task for task in tasks["tasks"] if "NOTRATED" in task]
 
-
     def rate_task(self, task_id, accepted, comment):
-        response = mw_api.do_request('PUT', '/campaign_hg/rate_task/' + self.campaign_id, {
-          'id_task': str(task_id),
-          'rating': 'OK' if accepted else 'NOK',
-          'comment': comment
-        })
+        response = mw_put('/campaign_hg/rate_task/' + self.campaign_id,
+                          {
+                              'id_task': str(task_id),
+                              'rating': 'OK' if accepted else 'NOK',
+                              'comment': comment
+                          })
         pprint(response)
+
+
+def get_task_vcode(task_id):
+    task_info = mw_get('GET', '/campaign_hg/get_task_info/{0}'.format(task_id))
+    if task_info is not None and len(task_info["proof"]) > 0:
+        return task_info["proof"][0]
+
+
+def mw_get(url):
+    try:
+        response = json.loads(mw_api.do_request('GET', url))
+        if response["status"] is "SUCCESS":
+            return response
+    except ValueError:
+        return None
+
+
+def mw_put(url, content):
+    try:
+        response = json.loads(mw_api.do_request('PUT', url, content))
+        if response["status"] is "SUCCESS":
+            return response
+    except ValueError:
+        return None
--- jobs/scripts/rate_tasks.py
+++ jobs/scripts/rate_tasks.py
@@ -1,8 +1,9 @@
-from mergelist_validation import Chunk
-from mw_communication import Campaign
+from scripts.mergelist_validation import Chunk
+from scripts.mw_communication import Campaign
 
 
 campaign_id = '2ebd1883a3f7'
+
 
 def run(*args):
     campaign = Campaign(campaign_id)
