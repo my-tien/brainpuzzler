a67af95f13bacddfeb7b740dcd5d565646854386
my-tien <maytie@gmail.com>
2015-03-06 21:27:39 +0100

    get tasks from specific microworker

--- jobs/scripts/mw_communication.py
+++ jobs/scripts/mw_communication.py
@@ -5,12 +5,11 @@ from jobs.scripts.MW_API import MW_API
 
 
 mw_api = MW_API('af801b23166b3a7a80df6eaf7efa3031095d16e69572f910a5c88bf2f6cf7fd9')
-campaign_id = '821113b0c802'
+campaign_id = '2ebd1883a3f7'
 
 
 def get_tasks():
     return mw_get('/campaign_hg/list_tasks/' + campaign_id)["tasks"]
-
 
 def get_unrated_tasks():
     tasks = get_tasks()
@@ -22,6 +21,10 @@ def get_accepted_tasks():
     tasks = get_tasks()
     if tasks is not None:
         return [task for task in tasks if "OK" in task]
+
+
+def ge_tasks_from(mw_id):
+    return mw_get('/campaign_hg/get_worker_tasks/' + campaign_id + "_" + mw_id)
 
 
 class Task:
