5c6bc60abe7185b42f797f05eb0311d538c27bcf
my-tien <maytie@gmail.com>
2015-03-10 19:56:36 +0100

    majority vote only for finished chunks

--- jobs/scripts/majority_vote.py
+++ jobs/scripts/majority_vote.py
@@ -1,7 +1,9 @@
 __author__ = 'tieni'
 
+from django.db.models import Q
+
 from jobs.chunk import Chunk
-from jobs.models import Submission
+from jobs.models import job_exists, Submission
 from jobs.scripts.submission_validation import write_majority_vote_mergelist
 
 info_path = "/home/knossos/chunk_infos/"
@@ -10,15 +12,24 @@ info_path = "/home/knossos/chunk_infos/"
 def run(*args):
     if len(args) == 0:
         return
-    chunk_range = range(0, 2475) if "all" in args else [int(value) for value in args]
+    chunk_range = [num for num in range(0, 2475) if job_exists(num)] if "all" in args else [int(value) for value in args[:-1]]
+    overlap_list = []
     for chunk_number in chunk_range:
         chunk = Chunk(chunk_number)
         mergelists = []
         overlaps = chunk.get_overlapping_chunks()
-        for submission in Submission.objects.filter(job__chunk_number__in=overlaps):
-            mergelists.append(submission.mergelist())
-        if "all" in args and len(mergelists) < 20:
+        chunks = []
+        available_submits = Submission.objects.filter(Q(job__chunk_number__in=overlaps), ~Q(state=Submission.REJECTED))
+        if len(available_submits) != 27:
             continue
+        for submission in available_submits:
+            if submission.job.chunk_number not in chunks:
+                if submission.mergelist() is not None:
+                    mergelists.append(submission.mergelist())
+                    chunks.append(submission.job.chunk_number)
+        overlap_list.append("chunk {0}: {1} overlaps\n".format(chunk_number, len(mergelists)))
         print("Creating majority vote mergelist for {0}".format(chunk_number))
         write_majority_vote_mergelist(chunk_number, mergelists)
 
+    with open(args[-1], 'w') as overlaps_f:
+        overlaps_f.writelines(overlap_list)
