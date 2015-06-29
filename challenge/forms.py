3fdfaf0f40a384c95e55628813acf14a271f78cb
Seba <gnu.seba@outlook.com>
2015-06-29 18:51:00 +0200

    add placeholder to fields

--- challenge/forms.py
+++ challenge/forms.py
@@ -3,9 +3,12 @@ __author__ = 'tieni'
 from django import forms
 
 class ChallengeSubmissionForm(forms.Form):
-    challenger_name = forms.CharField(max_length=100)
-    mail_address = forms.EmailField(max_length=100)
+    challenger_name = forms.CharField(max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    mail_address = forms.EmailField(max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Your E-Mail'}))
+    challenger_time = forms.CharField(max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Computation time (hh:mm:ss)'}))
     challenge_file = forms.FileField()
     challenge_file.help_text = "Upload a .k.zip file with segmentation data"
     challenge_file.label = "Submission file (*.k.zip)"
-
