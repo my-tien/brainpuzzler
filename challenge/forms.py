0e6893fb2ec6a0c85f5d6b2545ce982c5aae151c
Seba <gnu.seba@outlook.com>
2015-07-05 20:24:41 +0200

    change placeholder text for computation time

--- challenge/forms.py
+++ challenge/forms.py
@@ -8,7 +8,7 @@ class ChallengeSubmissionForm(forms.Form):
     mail_address = forms.EmailField(max_length=100,
         widget=forms.TextInput(attrs={'placeholder': 'Your E-Mail'}))
     challenger_time = forms.CharField(max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'Computation time (hh:mm:ss)'}))
+        widget=forms.TextInput(attrs={'placeholder': 'hh:mm:ss'}))
     challenge_file = forms.FileField()
     challenge_file.help_text = "Upload a .k.zip file with segmentation data"
     challenge_file.label = "Submission file (*.k.zip)"
