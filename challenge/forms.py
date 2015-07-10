9cec7aeb35a9bf6a0bc4f9a293642fa958e79dd7
Seba <gnu.seba@outlook.com>
2015-07-10 17:49:50 +0200

    remove max_length from challenger_time field
    since it isn’t supported by a forms.DurationField object

--- challenge/forms.py
+++ challenge/forms.py
@@ -7,8 +7,13 @@ class ChallengeSubmissionForm(forms.Form):
         widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
     mail_address = forms.EmailField(max_length=100,
         widget=forms.TextInput(attrs={'placeholder': 'Your E-Mail'}))
-    challenger_time = forms.CharField(max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'hh:mm:ss'}))
+    challenger_time = forms.DurationField(
+        widget=forms.TextInput(attrs={'placeholder': 'DD HH:MM:SS'}))
     challenge_file = forms.FileField()
     challenge_file.help_text = "Upload a .k.zip file with segmentation data"
     challenge_file.label = "Submission file (*.k.zip)"
+
+    def verify_form(self):
+        """
+        """
+
