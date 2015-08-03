41e542623b0e5e35b608c4d1580d6f4afa54d2d0
Seba <gnu.seba@outlook.com>
2015-08-03 15:33:54 +0200

    check file type and size before uploading

--- challenge/forms.py
+++ challenge/forms.py
@@ -1,6 +1,10 @@
 __author__ = 'tieni'
 
 from django import forms
+from django.utils.translation import ugettext as _
+
+TASK_UPLOAD_FILE_TYPES = ['application/zip']
+TASK_UPLOAD_FILE_MAX_SIZE = 5242880
 
 class ChallengeSubmissionForm(forms.Form):
     challenger_name = forms.CharField(max_length=100,
@@ -13,7 +17,25 @@ class ChallengeSubmissionForm(forms.Form):
     challenge_file.help_text = "Upload a .k.zip file with segmentation data"
     challenge_file.label = "Submission file (*.k.zip)"
 
-    def verify_form(self):
-        """
-        """
+    def clean(self):
+        file = self.cleaned_data['challenge_file']
+        print("______")
+        print(file)
+
+        try:
+            if file:
+                file_type = file.content_type
+
+                if len(file.name.split('.')) == 1:
+                    raise forms.ValidationError(_('File type is not supported'))
+
+                if file_type in TASK_UPLOAD_FILE_TYPES:
+                    if file._size > TASK_UPLOAD_FILE_MAX_SIZE:
+                        raise forms.ValidationError(_('Please keep filesize under %s. Current filesize %s') % (filesizeformat(settings.TASK_UPLOAD_FILE_MAX_SIZE), filesizeformat(file._size)))
+                else:
+                    raise forms.ValidationError(_('File type is not supported'))
+        except AttributeError:
+            pass
+
+        return file
 
