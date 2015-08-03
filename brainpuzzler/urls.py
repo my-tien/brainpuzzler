2830cae355eafd911ff8bdcfa7ee90fa6483336a
Seba <gnu.seba@outlook.com>
2015-08-03 14:29:37 +0200

    rename EMSeg challenge's url

--- brainpuzzler/urls.py
+++ brainpuzzler/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^segem-challenge/', include('challenge.urls')),
+                       url(r'^emseg-challenge/', include('challenge.urls')),
                        url(r'^jobs/', include('jobs.urls')))
